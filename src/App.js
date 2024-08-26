2227214fbfa94977891f61c4f9673881ab464ef8
EUNBI <rachelyu.1025@gmail.com>
2024-08-26 16:48:48 +0900

    💄 반응형 style 추가적용

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SearchBox from './components/SearchBox';
 
 function App() {
   return (
-    <div className='px-8 py-6 space-y-2'>
+    <div className='px-8 py-6 space-y-2 sm:px-24 md:px-36 md:py-12 md:space-y-3 lg:px-52 xl:px-80'>
       <h1 className='font-bold text-2xl'>Phone Book</h1>
 
       <CreateBox />
