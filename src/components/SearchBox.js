a6307f8b25b123e1885d7d8280faefda13637d23
EUNBI <rachelyu.1025@gmail.com>
2024-08-26 16:25:57 +0900

    ✨ SearchBox component UI

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,7 +1,24 @@
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
 import React from 'react';
 
 const SearchBox = () => {
-  return <div></div>;
+  return (
+    <div className='flex flex-col'>
+      <label htmlFor='search' className='font-bold text-lg'>
+        Search
+      </label>
+
+      <div className='flex items-center my-2'>
+        <input
+          type='text'
+          id='search'
+          name='search'
+          className='border-4 border-blue rounded-3xl bg-gray px-2 py-1 mr-2 w-full focus:outline-none'
+        />
+        <MagnifyingGlassIcon className='w-10 h-8 text-blue hover:cursor' />
+      </div>
+    </div>
+  );
 };
 
 export default SearchBox;
