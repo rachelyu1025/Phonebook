51cc43c9f27b6961d026536ba897b26276b84db8
EUNBI <rachelyu.1025@gmail.com>
2024-08-26 16:05:34 +0900

    ✨ CreateBox component UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import CreateBox from './components/CreateBox';
+import List from './components/List';
+import SearchBox from './components/SearchBox';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='px-8 py-8'>
+      <h1 className='font-bold text-2xl'>Phone Book</h1>
+
+      <CreateBox />
+      <SearchBox />
+      <List />
     </div>
   );
 }
---
+++ src/components/CreateBox.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const CreateBox = () => {
+  return (
+    <div className='flex flex-col rounded-xl my-4 py-3 px-6 bg-[#3f51e3] space-y-2 text-white'>
+      <label htmlFor='name' className=''>
+        Name
+      </label>
+      <input
+        type='text'
+        id='name'
+        name='name'
+        required
+        className='rounded focus:outline-none text-black p-1'
+      />
+
+      <label htmlFor='mobile' className=''>
+        Mobile
+      </label>
+      <input
+        type='tel'
+        id='mobile'
+        name='mobile'
+        pattern='[0-9]{3}-[0-9]{4}-[0-9]{4}'
+        required
+        className='rounded focus:outline-none p-1'
+      />
+      <span className='inline-block'>ex : 010-1234-5678</span>
+
+      <button
+        type='button'
+        className='font-bold text-lg self-end hover:cursor-pointer'
+      >
+        Add
+      </button>
+    </div>
+  );
+};
+
+export default CreateBox;
