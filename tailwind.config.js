ee2b4a96292f2914616d513fd56c7ac2f03d316a
EUNBI <rachelyu.1025@gmail.com>
2024-08-26 16:43:34 +0900

    ✨ screen size 기준 생성

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    screens: {
+      xs: '375px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
     colors: {
       white: '#ffffff',
       black: '#000000',
       blue: '#3f51e3',
     },
+    extend: {},
   },
   plugins: [],
 };
